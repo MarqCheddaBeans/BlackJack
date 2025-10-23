66665e2ec504bd46931113107d7fb488b2199757
IsaiahMV27 <mvillanueva@my.yearupunited.org>
2025-10-23 15:14:11 -0400

    Player class also added, allow user to enter amount of players and be dealt 2 cards each

--- src/main/java/com/pluralsight/GamblingTable.java
+++ src/main/java/com/pluralsight/GamblingTable.java
@@ -9,31 +9,58 @@ public class GamblingTable {
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        //Create list to store all players
         List<Player> gamblers = new ArrayList<>();
 
+        //Each player will also need a hand
+        List<Hand> hands = new ArrayList<>();
+
         System.out.println("Lets feed that gambling addiction with some Blackjack!");
+
+        //Prompt user for player count and store
         System.out.print("How many players? ");
         int numOfPlayers = scan.nextInt();
+
+        //Hungry buffer
         scan.nextLine();
 
+        //Ask for names of each player and create individual objects for each
         for(int i = 1; i <= numOfPlayers ; ++i){
             System.out.print("Enter name for player " + i + " ");
             String name = scan.nextLine();
             gamblers.add(new Player(name));
+            hands.add(new Hand());
             System.out.println("\nWelcome " + name);
         }
+
+        //Display all players
         System.out.println("\nFollowing Players: ");
         for(Player p : gamblers){
             System.out.println( p.getName());
         }
 
-        //create the deck
+        //create and shuffle the deck
         Deck deck = new Deck();
         deck.shuffle();
 
-        //create the hand
-        Hand hand1 = new Hand();
+        //Cycle through each player
+        for (int i = 0; i<numOfPlayers; i++){
+            //Deal 2 cards per player
+            for (int m = 0; m < 2; m++){
+                //Calls Deck deal() method, store card in variable named dealtCard
+                Card dealtCard = deck.deal();
+                //Flips card to reveal actual values
+                dealtCard.flip();
+                //Adds dealt card to player hand
+                hands.get(i).deal(dealtCard);
+
+                System.out.println("Cards dealt: \n" + dealtCard.getSuit() + " " + dealtCard.getValue());
+            }
+        }
+
+
+
+
 
 
 
